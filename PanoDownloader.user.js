70fbd522e8c5a0b2afa3fb87e612b8c51d30bd75
JDipi <turtlechampy@gmail.com>
2023-01-29 10:43:05 -0800

    made download window and added config files

--- PanoDownloader.user.js
+++ PanoDownloader.user.js
@@ -7,9 +7,8 @@
 // @match        https://*.google.com/local/imagery/report/*
 // @require      https://code.jquery.com/jquery-3.6.3.min.js
 // @require      https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js
-// @require      https://code.jquery.com/ui/1.13.1/jquery-ui.min.js
 // @resource     PANOVIEW https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css
-// @resource     UICSS https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css
+// @require      file:///C:/Users/turtl/OneDrive/Desktop/Google-Maps-Pano-Downloader/PanoDownloader.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=example.com
 // @grant        GM_addStyle
 // @grant        GM_getResourceText
@@ -21,6 +20,40 @@
 // tiles: w7 h4 (x 0-6, y 0-3)
 
 (function () {
+  const getStreetViewPano = (key) => {
+    for (let i = 0; i < 16; i++) {
+      for (let j = 0; j < 32; j++) {
+        $.ajax({
+          type: "GET",
+          url: `https://streetviewpixels-pa.googleapis.com/v1/tile?panoid=${key}&x=${j}&y=${i}&zoom=5`,
+          beforeSend: function (xhr) {
+            xhr.overrideMimeType("text/plain; charset=x-user-defined");
+          },
+          success: function (result, textStatus, jqXHR) {
+            if (result.length < 1) {
+              alert("The thumbnail doesn't exist");
+              $("#thumbnail").attr("src", "data:image/png;base64,");
+              return;
+            }
+
+            var binary = "";
+            var responseText = jqXHR.responseText;
+            var responseTextLen = responseText.length;
+
+            for (let k = 0; k < responseTextLen; k++) {
+              binary += String.fromCharCode(responseText.charCodeAt(k) & 255);
+            }
+
+            $(`<img id="panoTile" src="data:image/png;base64,${btoa(binary)}" x="${i}" y="${j}" style="grid-area: ${i + 1} / ${j + 1} / span 1 / span 1; width: 200px;" />`).appendTo("#image_div");
+          },
+          error: function (xhr, textStatus, errorThrown) {
+            console.log("whoops");
+          },
+        });
+      }
+    }
+  };
+
   const getPhotoSpherePano = (key) => {
     // CHECK IF THIS WIDTH IS UNIVERSAL, current max???
     let url = $("#preview-image").attr("src").replace(/=w.*/gm, "=w16383");
@@ -45,12 +78,8 @@
           binary += String.fromCharCode(responseText.charCodeAt(k) & 255);
         }
 
-        $(
-          `<img id="panoTile" src="data:image/png;base64,${btoa(
-            binary
-          )}" style="grid-area: 1 / 1 / span 1 / span 1;" />`
-        ).appendTo("#image_div");
-
+        $(/*html*/ `<img id="panoTile" src="data:image/png;base64,${btoa(binary)}" style="grid-area: 1 / 1 / span 1 / span 1;" />`).appendTo("#image_div");
+        $("#imgModal").show();
         fetch(`data:image/png;base64,${btoa(binary)}`)
           .then((res) => res.blob())
           .then((blob) => {
@@ -68,69 +97,61 @@
     });
   };
 
-  GM_addStyle(`
+  GM_addStyle(/*css*/ `
     #image_div {
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
-        grid-template-rows: 1fr 1fr 1fr;
+      display: grid;
+      position: relative;
+      width: 100%;
+      height: 50%;
+      background: white;
+    }
+
+    #image_div > img {
+      position: absolute;
+      height: 100%;
+      transform: translate(-50%, 0px);
+      left: 50%;
     }
 
     #download {
-        padding: 5px;
-        color: white;
+      padding: 5px;
+      color: white;
     }
 
-        #panorama {
-        width: 1200px;
-        height: 800px;
+    #panorama {
+      width: 100%;
+      height: 50%;
     }
-    `);
+
+    #imgModal {
+      position: fixed;
+      display: flex;
+      flex-direction: column;
+      top: 50px;
+      left: 40px;
+      bottom: 50px;
+      right: 40px;
+      z-index: 1000002;
+      background: gray;
+      justify-content: center;
+      align-items: center;
+    }
+
+  `);
 
   $(`<button id="download">Download Pano</button>`).prependTo("c-wiz");
 
-  $(`
-    <div id="dialog-confirm" title="Empty the recycle bin?">
-    <div id="image_div"></div>
-    <p>testttttt</p>
-  </div>
-    `).insertAfter(".O3cKLb");
-  $("#dialog-confirm").dialog({
-    resizable: false,
-    height: "auto",
-    width: 400,
-    modal: true,
-    buttons: {
-      "Delete all items": function () {
-        $(this).dialog("close");
-      },
-      Cancel: function () {
-        $(this).dialog("close");
-      },
-    },
-  });
-
-  $("#dialog-confirm").dialog({
-    resizable: false,
-    height: "auto",
-    width: 400,
-    modal: true,
-    buttons: {
-      "Delete all items": function () {
-        $(this).dialog("close");
-      },
-      Cancel: function () {
-        $(this).dialog("close");
-      },
-    },
-  });
+  $(/*html*/ `
+    <div id="imgModal">
+      <div id="image_div"></div>
+      <div id="panorama"></div>
+    </div>
+  `).insertAfter(".O3cKLb");
+  $("#imgModal").hide();
 
   GM_addStyle(GM_getResourceText("PANOVIEW"));
-  GM_addStyle(GM_getResourceText("UICSS"));
-
-  $(`<div id="panorama"></div>`).insertAfter(".GUPete");
 
   $("#download").on("click", () => {
-    console.log("hi");
     let key = window.location.href.match(/%212s(.*)/)[1];
     if ($("#streetview-preview").length) {
       getStreetViewPano(key);
