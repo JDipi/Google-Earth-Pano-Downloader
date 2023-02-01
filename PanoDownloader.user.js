e04deb76d5372e65b54c7e2ece2ce1e220971a15
JDipi <turtlechampy@gmail.com>
2023-01-31 22:07:35 -0800

    got rid of modal, added normal streetview support

--- PanoDownloader.user.js
+++ PanoDownloader.user.js
@@ -2,14 +2,13 @@
 // @name         Google Maps Pano Downloader
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Downloads Google Earth Streetview panoramas
 // @author       You
 // @match        https://*.google.com/local/imagery/report/*
 // @require      https://code.jquery.com/jquery-3.6.3.min.js
-// @require      https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.2.0/jszip.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.0/FileSaver.min.js
-// @resource     PANOVIEW https://cdn.jsdelivr.net/npm/pannellum@2.5.6/build/pannellum.css
+// @require      https://unpkg.com/merge-images@2.0.0/dist/index.umd.js
 // @require      file:///C:/Users/turtl/OneDrive/Desktop/Google-Maps-Pano-Downloader/PanoDownloader.user.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=example.com
 // @grant        GM_addStyle
@@ -22,8 +21,6 @@
 // https://www.google.com/local/imagery/report/?cb_client=earth.iv&cbp=1,0,,0,0&gl=US&hl=en&image_key=%211e2%212s7q_90B0sQurvz9YWoQtClQ
 // tiles: w7 h4 (x 0-6, y 0-3)
 
-// TODO: make sure file extensions are correct for final image
-
 /*
 i < 16
 j < 32
@@ -31,7 +28,6 @@ j < 32
 
 (function () {
   function getStreetViewPano(key) {
-    $(`<div id="images"></div>`).appendTo("body");
     let images = [];
     for (let i = 0; i < 16; i++) {
       for (let j = 0; j < 32; j++) {
@@ -56,9 +52,6 @@ j < 32
             for (let k = 0; k < responseTextLen; k++) {
               binary += String.fromCharCode(responseText.charCodeAt(k) & 255);
             }
-
-            // $(`<img id="panoTile" src="data:image/jpg;base64,${btoa(binary)}" x="${i}" y="${j}" style="grid-area: ${i + 1} / ${j + 1} / span 1 / span 1; width: 200px;" />`).appendTo("#images");
-            // $(`<img id="panoTile" src="data:image/jpg;base64,${btoa(binary)}" x="${i}" y="${j}" style="grid-area: ${i + 1} / ${j + 1} / span 1 / span 1; width: 200px;" />`).appendTo("#image_div");
             let src = `data:image/jpg;base64,${btoa(binary)}`;
             images.push({ src, x: j, y: i });
           },
@@ -66,13 +59,13 @@ j < 32
             console.log("whoops");
           },
         });
+        console.log(`Getting Tile: [${images.length}/512]`);
       }
     }
     return images;
   }
 
   const getPhotoSpherePano = (key) => {
-    // CHECK IF THIS WIDTH IS UNIVERSAL, current max???
     let url = $("#preview-image").attr("src").replace(/=w.*/gm, "=w16383");
     $.ajax({
       type: "GET",
@@ -95,16 +88,10 @@ j < 32
           binary += String.fromCharCode(responseText.charCodeAt(k) & 255);
         }
 
-        $("#imgModal").show();
         fetch(`data:image/jpg;base64,${btoa(binary)}`)
           .then((res) => res.blob())
           .then((blob) => {
-            pannellum.viewer("panorama", {
-              type: "equirectangular",
-              panorama: URL.createObjectURL(blob),
-              autoRotateInactivityDelay: 3000,
-              autoRotate: -5,
-            });
+            saveAs(blob, `${$(".R5MShd.Mqf7lf").text()}.png`);
           });
       },
       error: function (xhr, textStatus, errorThrown) {
@@ -114,6 +101,18 @@ j < 32
   };
 
   GM_addStyle(/*css*/ `
+
+    .outsideCLick {
+      position: fixed;
+      width: 100vw;
+      height: 100vh;
+      top: 0;
+      left: 0;
+      opacity: 50%;
+      background-color: gray;
+      z-index: -1;
+    }
+
     #image_div {
       display: grid;
       position: relative;
@@ -166,54 +165,48 @@ j < 32
       height: 100%;
     }
 
+    .test {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      overflow: scroll;
+    }
 
   `);
 
   $(`<button id="download">Download Pano</button>`).prependTo("c-wiz");
 
-  $(/*html*/ `
-    <div id="imgModal">
-      <div id="image_div"></div>
-      <div id="panorama"></div>
-    </div>
-  `).insertAfter(".O3cKLb");
-  $("#imgModal").hide();
-
-  GM_addStyle(GM_getResourceText("PANOVIEW"));
-
   function downloadImages(imgs) {
     let dirName = $(".R5MShd.Mqf7lf").text();
     var zip = new JSZip();
     imgs.forEach((el) => {
-      // GM_download({
-      //   url: el.src,
-      //   name: `${dirName}/${el.x}-${el.y}.jpg`,
-      //   onload: () => {
-      //     console.log(`finished: ${dirName}/${el.j}-${el.i}.jpg`);
-      //   },
-      // });
-      zip.file(`${el.x}-${el.y}.jpg`, el.src.split(",")[1], {base64: true});
+      zip.file(`${el.x}-${el.y}.jpg`, el.src.split(",")[1], { base64: true });
     });
-    zip.generateAsync({ type: "blob" }).then(content => saveAs(content, `${dirName}.zip`));
+    zip.generateAsync({ type: "blob" }).then((content) => saveAs(content, `${dirName}.zip`));
   }
 
   $("#download").on("click", () => {
     let key = window.location.href.match(/%212s(.*)/)[1];
     if ($("#streetview-preview").length) {
       let images = getStreetViewPano(key);
-      let w = 0
-      let h = 0
-      images.forEach(el => {
-        if (el.x+1 > w) w = el.x+1
-        if (el.y+1 > h) h = el.y+1
-        $(`<img id="panoTile" src="${el.src}" x="${el.x}" y="${el.y}" style="grid-area: ${el.y + 1} / ${el.x + 1} / span 1 / span 1; width: 100%; object-fit: cover;"/>`).appendTo("#image_div");
-      })
-      $("#image_div").css({"grid-template-rows": `repeat(${h}, 27px)`, "grid-template-columns": `repeat(${w}, 27px)`})
-      $("#imgModal").show();
+      let spaced = [];
+      images.forEach((el) => {
+        spaced.push({ src: el.src, x: el.x * 512, y: el.y * 512 });
+      });
+      console.log("Stitching Tiles...");
+      mergeImages(spaced, { width: 16384, height: 8192 }).then((b) => {
+        fetch(b)
+          .then((res) => res.blob())
+          .then((blob) => {
+            console.log("Downloading...");
+            saveAs(blob, `${$(".R5MShd.Mqf7lf").text()}.png`);
+          });
+      });
       // downloadImages(images);
     } else {
       getPhotoSpherePano(key);
     }
-    console.log(key);
   });
 })();
